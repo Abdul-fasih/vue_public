a5fc00ed103cd9316ee57f135d97da4e7ba8b3bc
Abdul-fasih <141391058+Abdul-fasih@users.noreply.github.com>
2023-08-21 18:41:53 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 1 & check slack notification"/>
+    <HelloWorld msg="Welcome to Your Vue.js App 1 and check slack notification"/>
   </div>
 </template>
 
